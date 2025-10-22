6100b32ce4e5d6ed62e5506cf3cc87eb96c61467
Gowsika V <gowsikavenkatachslam0713@gmail.com>
2025-10-22 12:36:03 +0530

    feat: enhance supplier dashboard with payment information and payment mail summary to admin on that payment date

--- farm-fresh/src/main/java/com/xworkz/farmfresh/config/PaymentScheduler.java
+++ farm-fresh/src/main/java/com/xworkz/farmfresh/config/PaymentScheduler.java
@@ -1,11 +1,15 @@
 package com.xworkz.farmfresh.config;
 
 import com.xworkz.farmfresh.service.PaymentNotificationService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
+
 @Component
+@Slf4j
 public class PaymentScheduler {
 
     @Autowired
@@ -17,10 +21,34 @@ public class PaymentScheduler {
         paymentNotificationService.generateAdvanceNotifications();
     }
 
-    // 0 0 9 15,30 * *  every month 15 and 30 at 9 AM     // 0 */1 * * * *
-    @Scheduled(cron = "0 0 9 15,30 * *", zone = "Asia/Kolkata")
+    // every day at 9 AM    // 0 */1 * * * *
+    @Scheduled(cron = "0 0 9 * * *", zone = "Asia/Kolkata")
     public void runPaymentNotification() {
-        paymentNotificationService.generatePaymentNotifications();
+        LocalDate today = LocalDate.now();
+        int dayOfMonth = today.getDayOfMonth();
+        int lastDayOfMonth = today.lengthOfMonth();
+
+        if (dayOfMonth == 15 || dayOfMonth == lastDayOfMonth) {
+            paymentNotificationService.generatePaymentNotifications();
+        } else {
+            log.info("Today ({}) is not 15th or last day ({}), skipping payment notification.",
+                    dayOfMonth, lastDayOfMonth);
+        }
+    }
+
+    @Scheduled(cron = "0 0 18 * * *", zone = "Asia/Kolkata") // every day at 6 PM
+    public void runEveningPaymentSummary() {
+        LocalDate today = LocalDate.now();
+        int dayOfMonth = today.getDayOfMonth();
+        int lastDayOfMonth = today.lengthOfMonth();
+
+        if (dayOfMonth == 15 || dayOfMonth == lastDayOfMonth) {
+            if(paymentNotificationService.getPaymentDetailsForAdminEmailSummary())
+                log.info("Email summary send to admins");
+        } else {
+            log.info("Today ({}) is not 15th or last day ({}), skipping admin payment summary.",
+                    dayOfMonth, lastDayOfMonth);
+        }
     }
 
 }
